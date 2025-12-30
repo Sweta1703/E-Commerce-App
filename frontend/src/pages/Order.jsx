2237778ee620f777ca0a50a3dc3359f81bc7e64c
Sweta1703 <mondalsweta03@gmail.com>
2025-12-30 22:10:13 +0530

    Fix case-sensitivity build error

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,50 +1,50 @@
-import React,{ useContext,useState } from 'react'
-import { ShopContext } from '../context/shopContext';
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
 import Title from '../components/Title';
 import { useEffect } from 'react';
 import axios from 'axios';
 
 const Order = () => {
-  const { backendUrl, token, currency} = useContext(ShopContext);
-  const[orderData,setOrderData] = useState([]);
+  const { backendUrl, token, currency } = useContext(ShopContext);
+  const [orderData, setOrderData] = useState([]);
 
   const loadOrderData = async () => {
     try {
-      if(!token) return;
-      const response = await axios.post(backendUrl + '/api/order/userorders',{},{headers:{token}})
-      if(response.data.success){
-        let allOrdersItem =[]
+      if (!token) return;
+      const response = await axios.post(backendUrl + '/api/order/userorders', {}, { headers: { token } })
+      if (response.data.success) {
+        let allOrdersItem = []
         response.data.orders.map((order) => {
           order.items.map((item) => {
-            item ['status'] = order.status;
-            item ['payment'] = order.payment;
-            item ['paymentMethod'] = order.paymentMethod;
-            item ['date'] = order.date;
+            item['status'] = order.status;
+            item['payment'] = order.payment;
+            item['paymentMethod'] = order.paymentMethod;
+            item['date'] = order.date;
             allOrdersItem.push(item);
           })
         })
         setOrderData(allOrdersItem.reverse());
       }
     } catch (error) {
-      
+
     }
   }
 
   useEffect(() => {
     loadOrderData();
-  },[token])
+  }, [token])
 
   return (
     <div className='border-t pt-16'>
       <div className='text-2xl'>
-        <Title text1={"MY"} text2={"ORDERS"}/>
+        <Title text1={"MY"} text2={"ORDERS"} />
       </div>
       <div>
         {
-          orderData.map((item,index) => (
+          orderData.map((item, index) => (
             <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
               <div className='flex items-start gap-6 text-sm'>
-                <img className='w-16 sm:w-20' src={item.image[0]} alt=""/>
+                <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
                 <div>
                   <p className='sm:text-base font-medium'>{item.name}</p>
                   <div className='flex items-center gap-3 mt-1 text-base text-gray-700'>
